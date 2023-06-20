6c40dca35c30aee40405941c3b1e66c610e864f8
Utkarsh-190 <utkarshtyagi62@gmail.com>
2023-06-20 18:20:06 +0530

    added basic layout

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import {Task} from "../../Task";
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task: Task = {
+    text: "",
+    day: "",
+    reminder: false,
+  };
+}
